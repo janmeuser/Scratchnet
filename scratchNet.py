23f3603790269191bef66cf72a6a2e7e050504b0
janmeuser <meuser.jan@web.de>
2023-11-30 10:45:30 +0100

    some change

--- scratchNet.py
+++ scratchNet.py
@@ -14,9 +14,9 @@ def plot_loss_and_accuracy(losses, accuracies, xlabel):
     plt.plot(accuracies, label="accuracy")
     plt.legend(loc="upper left")
     plt.xlabel(xlabel)
-    plt.ylim(top=1. bottom=0)
+    plt.ylim(top=1, bottom=0)
     plt.show()
-    
+
 class SquadError:
     raise NotImplementedError()
 
@@ -27,16 +27,27 @@ class DifferentiableFunction(abc.ABC):
         pass
 
 class Sigmoid(DifferentiableFunction):
-    def drivative(self, net_input):
-        return self(net_input) * (1 - self_input))
-    
+    def derivative(self, net_input):
+        return self(net_input) * (1 - self(net_input))
+
     def __call__(self, net_input):
         return 1 / (1 + np.exp(-net_input))
 
 class ScratchNet:
     #methods
     def __init__(self, layers):
-        raise NotImplementedError()
+        self.learning_rate = 0.5
+        self.cost_func = SquaredError()
+        self.layers = layers
+        # for Schleife um durch index zu iterieren und Layer zu verketten 
+        for index, layer in enumerate(self.layers):
+            layer.prev_layer = self.layers[index - 1] if index > 0 else None
+            layer.next_layer = (
+                self.layers[index + 1] if index +
+                1 < len(self.layers) else None
+            )
+            layer.depth = index
+            layer.initialize_parameters()
 
     def fit(self, train_images, train_labels, epochs=1):
         raise NotImplementedError()
@@ -52,6 +63,25 @@ class ScratchNet:
 
     def inspect(self):
         raise NotImplementedError()
+    
+class DenseLayer: #initialize Layer
+    def __init__(
+        self,
+        neuron_count,
+        depth=None,
+        activation=None,
+        biases=None,
+        weights=None,
+        prev_layer=None,
+        next_Layer=None,
+    ):
+        raise NotImplementedError()
+    
+    def initialize_parameters(self):
+        if self.weights is None:
+            self.weights = np.random.randn(self.neuron_count, self.prev_layer.neuron_count)
+        if self.biases is None:
+            self.biases = np.randomnrandn(self.neuron_count, 1)
 
 def xor():
     train_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
@@ -62,25 +92,25 @@ def xor():
     
     model = ScratchNet(
         [
-            FlattenLayer(input_shape=2, 1)),
-            DenseLayer(4, activation=Sigmoid()),
-            DenseLayer(2, activation=Sigmoid()),
+            DenseLayer(neuron_count=4, activation=Sigmoid()),
+            DenseLayer(neuron_count=2, activation=Sigmoid()),
         ]
     )
     
-# repeat the values
-repeat = (1000, 1)
-train_inputs = np.tile(train_inputs, repeat)
-train_vec_labels = np.tile(train_vec_labels, repeat)
+    # repeat the values
+    repeat = (1000, 1)
+    train_inputs = np.tile(train_inputs, repeat)
+    train_vec_labels = np.tile(train_vec_labels, repeat)
 
-model.compile(learning_rate=0.1, loss=SquaredError())
+    model.compile(learning_rate=0.1, loss=SquaredError())
 
-start = time.time()
-losses, accurcaies = model.fit(
-    train_inputs, train_vec_labels, epochs=5)
-end = time.time()
-print('Trainingsdauer: {:.lf}s'.format(end-start))
+    start = time.time()
+    losses, accuracies = model.fit(
+        train_inputs, train_vec_labels, epochs=5
+    )
+    end = time.time()
+    print('Trainingsdauer: {:.1f}s'.format(end - start))
 
-val_loss, val_acc = model.evaluate(
-    validation_images=train_inputs, validation_labels=train_vec_labels
-)
+    val_loss, val_acc = model.evaluate(
+        validation_images=train_inputs, validation_labels=train_vec_labels
+    )
